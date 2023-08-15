3a613d6933930a113487d08ae2bedf6528d027ab
MKYRENE <rodriguezm90@icloud.com>
2023-08-15 11:55:58 -0400

    updated contact and about page implement roles and github pages for each dev.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
+//Team members 
+const teamMembers = [
+    {
+        name: 'Francis Reyes',
+        role: 'Backend Developer',
+        github: 'https://github.com/Reyesfrancisp'
+    },
+    {
+        name: 'Michael Rodriguez',
+        role: 'Frontend Developer',
+        github: 'https://github.com/mkyrene'
+    },
+    {
+        name: 'Isaias F',
+        role: 'Frontend Developer',
+        github: 'https://github.com/IsaiasD18'
+    },
+    {
+        name: 'Liam Brandt',
+        role: 'Backend Developer',
+        github: 'https://github.com/LiamG00250118'
+    },
+];
+
 function About() {
     return (
         <div>
@@ -8,6 +32,17 @@ function About() {
             <p>Each member of our team brings unique skills, experiences, and perspectives to the table, contributing to our collaborative and dynamic work environment.</p>
             <p>Whether it's design, development, or any other aspect of our projects, we're committed to delivering the best results and pushing the boundaries of what we can achieve.</p>
             <p>Feel free to explore our profiles and get to know us better. We're excited to have you on this journey with us!</p>
+
+            <h2>Meet Our Team</h2>
+            <ul>
+                {teamMembers.map((member, index) => (
+                    <li key={index}>
+                        <strong>{member.name}</strong> - {member.role}
+                        <br />
+                        GitHub: <a href={member.github} target="_blank" rel="noopener noreferrer">{member.github}</a>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
