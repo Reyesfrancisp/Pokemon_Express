8a388110cdd0b425e34ad16a6f9a898d1f7a8d0b
Liamb44 <liamrbrandt@gmail.com>
2023-08-15 12:32:43 -0400

    test

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import getPokemonInfo from './pokeApiQuery';
-import PokemonCard from '../../components/PokemonCard';
+import PokemonCard from '../../components/pokemonCard.js';
 function Search() {
   const [searchQuery, setSearchQuery] = useState('');
   const [info, setInfo] = useState({
