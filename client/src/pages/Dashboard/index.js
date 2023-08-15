68d5787bc8ba97be21989e9390a726325ebf62cd
MKYRENE <rodriguezm90@icloud.com>
2023-08-15 11:18:12 -0400

    updated contact and about me page setting up templates for page information.

---
+++ client/src/pages/Dashboard/index.js
@@ -0,0 +1,12 @@
+function Dashboard () {
+
+    return (
+      <main className="dashboard">
+        <h1 className="text-center">Welcome to the Pokemon Devdex</h1>
+  
+      
+      </main>
+    )
+  }
+  
+  export default Dashboard;
---
+++ client/src/pages/Dashboard/mutations.js
@@ -0,0 +1,12 @@
+function Mutations () {
+
+    return (
+      <main className="dashboard">
+        <h1 className="text-center">Welcome to the Pokemon Devdex</h1>
+  
+      
+      </main>
+    )
+  }
+  
+  export default Mutation;
