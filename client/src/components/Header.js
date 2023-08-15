c9669c7b850e1b26dc8bc112e481fa70cdc4e2be
IsaiasD18 <133791406+IsaiasD18@users.noreply.github.com>
2023-08-15 13:48:04 -0400

    Added the /seach on the nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 
-
 function Header(props) {
   const logout = async e => {
     e.preventDefault();
@@ -12,9 +11,9 @@ function Header(props) {
       return {
         ...oldState,
         user: null
-      }
-    })
-  }
+      };
+    });
+  };
 
   return (
     <header className="flex justify-between items-center py-4 px-6 bg-white border-b dark:bg-gray-900">
@@ -22,43 +21,33 @@ function Header(props) {
 
       <nav className="flex items-center space-x-4">
         {props.state.user && <p className="text-gray-600">Welcome, {props.state.user.username}</p>}
-        <NavLink
-          to="/"
-          className="text-white-600 hover:text-gray-800"
-         
-        >
+        <NavLink to="/" className="text-white-600 hover:text-gray-800">
           Home
+        </NavLink>
+        <NavLink to="/search" className="text-white-600 hover:text-gray-800">
+          Pokemons
         </NavLink>
         {props.state.user ? (
           <>
-            <NavLink
-              to="/dashboard"
-              className="text-white-600 hover:text-gray-800"
-             
-            >
+            <NavLink to="/dashboard" className="text-white-600 hover:text-gray-800">
               Dashboard
             </NavLink>
             <NavLink
               onClick={logout}
               to="/logout"
               className="text-white-600 hover:text-gray-800"
-             
             >
               Log Out
             </NavLink>
           </>
         ) : (
-          <NavLink
-            to="/auth"
-            className="text-white-600 hover:text-gray-800"
-           
-          >
+          <NavLink to="/auth" className="text-white-600 hover:text-gray-800">
             Login
           </NavLink>
         )}
       </nav>
     </header>
-  )
+  );
 }
 
 export default Header;
