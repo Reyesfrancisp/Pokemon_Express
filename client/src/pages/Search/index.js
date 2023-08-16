e8e3ae13eb595610fb0d8740d08a6a1fff69b049
Francis Reyes <reyesfrancisp@gmail.com>
2023-08-16 11:46:37 -0400

    added buttons for left and right

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -66,19 +66,48 @@ function Search() {
         </button>
 
         <div className="flex justify-center items-center space-x-10 mt-4">
+
           <button
-            className="py-2 px-4 bg-blue-500 text-white hover:bg-blue-600 w-[150px]"
+            className="flex items-center justify-center py-2 px-4 bg-blue-500 text-white hover:bg-blue-600 w-[140px] border-2 border-blue-600 rounded-lg transition duration-300"
             onClick={previousPokemon}
           >
-            Previous
+            <svg
+              className="w-4 h-4 ml-2"
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path
+                d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+                fill="currentColor"
+              />
+            </svg>
+
+            <p className="mx-2">Previous</p>
           </button>
+
+
           <PokemonCard info={info} />
+
+
           <button
-            className="py-2 px-4 bg-blue-500 text-white hover:bg-blue-600 w-[150px]"
+            className="flex items-center justify-center py-2 px-4 bg-blue-500 text-white hover:bg-blue-600 w-[140px] border-2 border-blue-600 rounded-lg transition duration-300"
             onClick={nextPokemon}
           >
-            Next
+            <p className="mx-2">Next</p>
+            <svg
+              className="h-4 ml-2"
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path
+                d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+                fill="currentColor"
+              />
+            </svg>
           </button>
+
         </div>
       </div>
     </div>
