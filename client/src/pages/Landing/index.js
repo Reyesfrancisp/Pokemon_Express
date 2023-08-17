57a4ce0f702057eeead946714faee565a1ba0da4
IsaiasD18 <133791406+IsaiasD18@users.noreply.github.com>
2023-08-16 23:21:38 -0400

    Worked on the search, home and footer

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-function Landing() {
+function DancingPokemon({ pokemonImage }) {
   return (
-    <main className="landing bg-cover bg-center  h-screen flex items-center">
+    <div className="animate-bounce mb-4">
+      <img
+        src={pokemonImage}
+        alt="Dancing Pokemon"
+        className="w-16 h-16 mx-auto"
+      />
+    </div>
+  );
+}
+
+function Landing() {
+  const pokemonImage = 'https://th.bing.com/th/id/R.eda5531538101e2e5e3babbd75f811eb?rik=m857wQaMnapABw&riu=http%3a%2f%2forig08.deviantart.net%2f4dfb%2ff%2f2014%2f090%2f6%2f1%2fpikachu___01_by_mighty355-d7cdjy7.png&ehk=%2byUUP8C5NCRSYi0hJ3Lq2HpzY3uaqw2AKbH0JyCGFik%3d&risl=&pid=ImgRaw&r=0'; 
+
+  return (
+    <main className="landing bg-cover bg-center h-screen flex items-center">
       <div className="mx-auto text-center">
+        <DancingPokemon pokemonImage={pokemonImage} />
         <h1 className="text-4xl md:text-6xl text-white font-bold mb-4">
           Welcome to the PokeDex Express
         </h1>
